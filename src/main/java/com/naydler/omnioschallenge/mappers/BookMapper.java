091bb0c1a88529d151a3cbd0fd928f30413ad632
gaalbesa <galbesa@alumnat.copernic.cat>
2023-10-18 12:15:26 +0200

    Mapped elements to empty dtos.

--- src/main/java/com/naydler/omnioschallenge/mappers/BookMapper.java
+++ src/main/java/com/naydler/omnioschallenge/mappers/BookMapper.java
@@ -1,17 +1,17 @@
 package com.naydler.omnioschallenge.mappers;
 
-import java.util.ArrayList;
-
+import java.util.List;
 import org.springframework.stereotype.Component;
-
 import com.naydler.omnioschallenge.dtos.BookDto;
 import com.naydler.omnioschallenge.entities.BookEntity;
 
 @Component
 public class BookMapper {
 
-    public ArrayList<BookDto> map(ArrayList<BookEntity> scrappedBooks) {
-        
+    public BookDto[] map(List<BookEntity> scrappedBooks) {
+        return scrappedBooks.stream().map(scrappedBook -> {
+            // TODO Obtain and transform data
+            return new BookDto();
+        }).toArray(BookDto[]::new);
     }
-    
 }
