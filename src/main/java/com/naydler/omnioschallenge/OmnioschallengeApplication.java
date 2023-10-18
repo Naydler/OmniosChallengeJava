f043ab5e49c65f81d4acca7dd321b32b464c5c20
gaalbesa <galbesa@alumnat.copernic.cat>
2023-10-18 10:57:38 +0200

    Push of generated springboot

---
+++ src/main/java/com/naydler/omnioschallenge/OmnioschallengeApplication.java
@@ -0,0 +1,13 @@
+package com.naydler.omnioschallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmnioschallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmnioschallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naydler/omnioschallenge/OmnioschallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naydler.omnioschallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmnioschallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
