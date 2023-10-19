53b2ee2af1e31debc0cf549d24f2937493a1ef04
gaalbesa <galbesa@alumnat.copernic.cat>
2023-10-19 11:38:00 +0200

    Created deepAi component

--- src/main/java/com/naydler/omnioschallenge/core/DeepAIManager.java
+++ src/main/java/com/naydler/omnioschallenge/core/DeepAIManager.java
@@ -1,34 +1,32 @@
 package com.naydler.omnioschallenge.core;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpRequest.BodyPublishers;
-import java.net.http.HttpResponse.BodyHandlers;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DeepAIManager {
     public String getText(String title) {
-        var client = HttpClient.newBuilder().build();
-        var question = "{\"text\":\"The text of "+ title +" is:\"}";
-        var request = HttpRequest.newBuilder().uri(URI.create("https://api.deepai.org/api/text-generator"))
-                .header("api-key", "quickstart-QUdJIGlzIGNvbWluZy4uLi4K").POST(BodyPublishers.ofString(question)).build();
-        HttpResponse<String> response;
+        var question = "{\"text\":\"The text of " + title + " is:\"}";
+
+        HttpURLConnection connection;
         try {
-            // Use question on the body handler
-            response = client.send(request, BodyHandlers.ofString());
-            System.out.println(response.statusCode());
-            System.out.println(response.body());
-            return response.body();
-        } catch (IOException e) {
+            connection = (HttpURLConnection) new URL("https://api.deepai.org/api/text-generator").openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("api-key", "quickstart-QUdJIGlzIGNvbWluZy4uLi4K");
+            connection.setDoOutput(true);
+
+            OutputStream os = connection.getOutputStream();
+            byte[] input = question.getBytes("utf-8");
+            os.write(input, 0, input.length);
+
+            return connection.getResponseCode() == HttpURLConnection.HTTP_OK 
+                ? "Dummy data due to a successful request"
+                : connection.getResponseMessage();
+        } catch (Exception e) {
             e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            return "There was an error with the request";
         }
-        
-        return "There was an error with the request";
     }
 }
